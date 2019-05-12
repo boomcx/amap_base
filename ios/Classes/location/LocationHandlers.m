1c7f66edfe4b83c98b8e3f34ebb7065eb4ff6dd4
th <707939932@qq.com>
2019-05-12 11:48:40 +0800

    iOS location 中增加协议方法，以获取定位权限

--- ios/Classes/location/LocationHandlers.m
+++ ios/Classes/location/LocationHandlers.m
@@ -81,6 +81,12 @@ static AMapLocationManager *_locationManager;
 
 }
 
+// 获取定位权限
+- (void)amapLocationManager:(AMapLocationManager *)manager doRequireLocationAuth:(CLLocationManager *)locationManager {
+    [locationManager requestAlwaysAuthorization];
+}
+
+
 - (void)amapLocationManager:(AMapLocationManager *)manager didUpdateLocation:(CLLocation *)location reGeocode:(AMapLocationReGeocode *)reGeocode {
     NSLog(@"location:{lat:%f; lon:%f; accuracy:%f}", location.coordinate.latitude, location.coordinate.longitude, location.horizontalAccuracy);
     if (_sink) {
